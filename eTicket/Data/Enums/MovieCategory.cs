3777778954599503d0ddb19e1721c18926cf024c
TanPhat0302 <phatt.maii01@gmail.com>
2023-05-10 19:35:11 +0300

    Update

--- eTicket/Data/Enums/MovieCategory.cs
+++ eTicket/Data/Enums/MovieCategory.cs
@@ -5,6 +5,8 @@
         Action = 1,
         Coomedy,
         Drama,
-        Documentary
+        Documentary,
+        Horror,
+        Cartoon
     }
 }
--- eTicket/Models/Movie.cs
+++ eTicket/Models/Movie.cs
@@ -34,7 +34,6 @@ namespace eTickets.Models
         public int ProducerId { get; set; }
 
         [ForeignKey("ProducerId")]
-
         public Producer Producers { get; set; }
     }
 }
